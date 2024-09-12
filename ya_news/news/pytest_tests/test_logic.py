1eaebf8ca1d900d03e0c4715dc7c903621f5cdd5
Karramb <ap.vecna@gmail.com>
2024-09-13 02:06:26 +0300

    Update test_logic.py

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -5,7 +5,7 @@ from pytest_django.asserts import assertRedirects, assertFormError
 from django.urls import reverse
 
 from news.forms import BAD_WORDS, WARNING
-from news.models import Comment, News
+from news.models import Comment
 
 COMMENT_TEXT = 'Текст комментария'
 NEW_COMMENT_TEXT = 'Обновлённый комментарий'
@@ -19,6 +19,7 @@ def test_anonymous_user_cant_create_comment(client, news):
     comments_count = Comment.objects.count()
     assert comments_count == 0
 
+
 @pytest.mark.django_db
 def test_user_can_create_comment(author_client, news):
     url = reverse('news:detail', args=(news.id,))
@@ -29,6 +30,7 @@ def test_user_can_create_comment(author_client, news):
     assert comments_count == 1
     comment = Comment.objects.last()
     assert comment.text == form_data['text']
+
 
 @pytest.mark.django_db
 def test_user_cant_use_bad_words(author_client, news):
@@ -44,6 +46,7 @@ def test_user_cant_use_bad_words(author_client, news):
     comments_count = Comment.objects.count()
     assert comments_count == 0
 
+
 def test_author_can_delete_comment(author_client, comment, news):
     response = author_client.delete(reverse('news:delete', args=(comment.id,)))
     news_url = reverse('news:detail', args=(news.id,))
@@ -58,6 +61,7 @@ def test_user_cant_delete_comment_of_another_user(not_author_client, comment):
     comments_count = Comment.objects.count()
     assert comments_count == 1
 
+
 def test_author_can_edit_comment(author_client, comment, news):
     edit_url = reverse('news:edit', args=(comment.id,))
     news_url = reverse('news:detail', args=(news.id,))
@@ -67,6 +71,7 @@ def test_author_can_edit_comment(author_client, comment, news):
     assertRedirects(response, url_to_comments)
     comment.refresh_from_db()
     assert comment.text == NEW_COMMENT_TEXT
+
 
 def test_user_cant_edit_comment_of_another_user(not_author_client, comment):
     edit_url = reverse('news:edit', args=(comment.id,))
