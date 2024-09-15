4b99e61e2dac89975d9a88e871a30f0fd047ee69
Karramb <ap.vecna@gmail.com>
2024-09-15 18:07:54 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.urls import reverse
 
 from notes.models import Note
@@ -14,62 +14,71 @@ class TestRoutes(TestCase):
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Лев Толстой')
         cls.reader = User.objects.create(username='Читатель простой')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.author)
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
         cls.note = Note.objects.create(
             title='Тест', text='Тест', slug='note_test', author=cls.author)
-
-    def test_pages_availability(self):
-        urls = (
-            ('notes:home', None),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
+        notes_home = reverse('notes:home', None)
+        users_login = reverse('users:login', None)
+        users_logout = reverse('users:logout', None)
+        users_signup = reverse('users:signup', None)
+        notes_list = reverse('notes:list', None)
+        notes_success = reverse('notes:success', None)
+        notes_add = reverse('notes:add', None)
+        cls.notes_detail = reverse('notes:detail', args=(cls.note.slug,))
+        cls.notes_edit = reverse('notes:edit', args=(cls.note.slug,))
+        cls.notes_delete = reverse('notes:delete', args=(cls.note.slug,))
+        cls.urls_for_all = (
+            notes_home,
+            users_login,
+            users_logout,
+            users_signup,
         )
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                response = self.client.get(url)
+        cls.urls_for_authorized_user = (
+            notes_list,
+            notes_success,
+            notes_add,
+            cls.notes_detail,
+            cls.notes_edit,
+            cls.notes_delete,
+        )
+        cls.all_urls = cls.urls_for_authorized_user + cls.urls_for_all
+        cls.LOGIN_URL = reverse('users:login')
+
+
+    def test_for_author(self):
+        """Автор может зайти на все страницы."""
+        for url in self.all_urls:
+            with self.subTest(user=self.author, url=url):
+                response = self.auth_client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_anonymous_user_cant_see_notes(self):
-        urls = [
-            ('notes:detail', (self.note.slug,)),
-            ('notes:list', None),
-            ('notes:success', None),
-            ('notes:add', None),
-            ('notes:edit', (self.note.slug,)),
-            ('notes:delete', (self.note.slug,))
-        ]
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                login_url = reverse('users:login')
-                redirect_url = f'{login_url}?next={url}'
-                response = self.client.get(url)
-                self.assertRedirects(response, redirect_url)
+    def test_for_reader(self):
+        """Обычный юзер может зайти на все страницы кроме чужих заметок."""
+        for url in self.urls_for_authorized_user:
+            with self.subTest(user=self.reader):
+                response = self.reader_client.get(url)
+                if url in (
+                    self.notes_detail,
+                    self.notes_edit,
+                    self.notes_delete
+                ):
+                    self.assertEqual(
+                        response.status_code,
+                        HTTPStatus.NOT_FOUND
+                    )
+                else:
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_user_can_see_or_create_or_edit_or_delete_only_self_note(self):
-        users_statuses = (
-            (self.author, HTTPStatus.OK),
-            (self.reader, HTTPStatus.NOT_FOUND),
-        )
-        for user, status in users_statuses:
-            self.client.force_login(user)
-            args = (self.note.slug,)
-            for name in ('notes:detail', 'notes:edit', 'notes:delete'):
-                with self.subTest(user=user, name=name):
-                    url = reverse(name, args=args)
-                    response = self.client.get(url)
-                    self.assertEqual(response.status_code, status)
-
-    def test_user_can_see_list_add_success_notes(self):
-        urls = (
-            ('notes:list', None),
-            ('notes:add', None),
-            ('notes:success', None)
-        )
-        for name, args in urls:
-            self.client.force_login(self.author)
-            with self.subTest(user=self.author, name=name):
-                url = reverse(name, args=args)
+    def test_for_anonim_user(self):
+        """Анониму доступны только страницы логина/регистрации и главная."""
+        for url in self.all_urls:
+            with self.subTest():
+                redirect_url = f'{self.LOGIN_URL}?next={url}'
                 response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
+                if (url) in self.urls_for_authorized_user:
+                    self.assertRedirects(response, redirect_url)
+                else:
+                    self.assertEqual(response.status_code, HTTPStatus.OK)
