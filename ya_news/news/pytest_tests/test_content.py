1fedd67bbf82cae0e87fba82cc7e22d570a8f412
Karramb <ap.vecna@gmail.com>
2024-09-15 18:06:17 +0300

    Update test_content.py

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -1,34 +1,32 @@
 import pytest
 from django.conf import settings
 
-from django.urls import reverse
-
 from news.forms import CommentForm
 
 
-HOME_URL = reverse('news:home')
-
-
-@pytest.mark.django_db
-def test_news_count(client, news_11):
-    response = client.get(HOME_URL)
+pytestmark = pytest.mark.django_db
+def test_news_count(client, home_url, news_11):
+    """На главной работает пагинация."""
+    response = client.get(home_url)
     object_list = response.context['object_list']
     news_count = object_list.count()
     assert news_count == settings.NEWS_COUNT_ON_HOME_PAGE
 
 
-@pytest.mark.django_db
-def test_news_order(client, news_11):
-    response = client.get(HOME_URL)
+pytestmark = pytest.mark.django_db
+def test_news_order(client, home_url, news_11):
+    """Новости отсортированы в правильном порядке."""
+    response = client.get(home_url)
     object_list = response.context['object_list']
     all_dates = [news.date for news in object_list]
     sorted_dates = sorted(all_dates, reverse=True)
     assert all_dates == sorted_dates
 
 
-@pytest.mark.django_db
-def test_comments_order(client, comments, news):
-    response = client.get(reverse('news:detail', args=(news.id,)))
+pytestmark = pytest.mark.django_db
+def test_comments_order(client, comments, detail_url, news):
+    """Комментарии отсортированы в правильном порядке."""
+    response = client.get(detail_url)
     news = response.context['news']
     comments = news.comment_set.all()
     all_timestamps = [comment.created for comment in comments]
@@ -36,13 +34,15 @@ def test_comments_order(client, comments, news):
     assert all_timestamps == sorted_timestamps
 
 
-@pytest.mark.django_db
-def test_anonymous_client_has_no_form(client, news):
-    response = client.get(reverse('news:detail', args=(news.id,)))
+pytestmark = pytest.mark.django_db
+def test_anonymous_client_has_no_form(client, detail_url, news):
+    """У анонима нет формы комментария."""
+    response = client.get(detail_url)
     assert 'form' not in response.context
 
 
-def test_authorized_client_has_form(author_client, news):
-    response = author_client.get(reverse('news:detail', args=(news.id,)))
+def test_authorized_client_has_form(author_client, detail_url, news):
+    """У авторизованного юзера есть форма комментария."""
+    response = author_client.get(detail_url)
     assert 'form' in response.context
     assert isinstance(response.context['form'], CommentForm)
