c86cca86f979bc811e3605300d2bb81e2f502d07
Karramb <ap.vecna@gmail.com>
2024-09-17 11:24:22 +0300

    Update test_routes.py

--- ya_note/notes/tests/test_routes.py
+++ ya_note/notes/tests/test_routes.py
@@ -20,13 +20,13 @@ class TestRoutes(TestCase):
         cls.reader_client.force_login(cls.reader)
         cls.note = Note.objects.create(
             title='Тест', text='Тест', slug='note_test', author=cls.author)
-        notes_home = reverse('notes:home', None)
-        users_login = reverse('users:login', None)
-        users_logout = reverse('users:logout', None)
-        users_signup = reverse('users:signup', None)
-        cls.notes_list = reverse('notes:list', None)
-        cls.notes_success = reverse('notes:success', None)
-        cls.notes_add = reverse('notes:add', None)
+        notes_home = reverse('notes:home')
+        cls.users_login = reverse('users:login')
+        users_logout = reverse('users:logout')
+        users_signup = reverse('users:signup')
+        cls.notes_list = reverse('notes:list')
+        cls.notes_success = reverse('notes:success')
+        cls.notes_add = reverse('notes:add')
         cls.notes_detail = reverse('notes:detail', args=(cls.note.slug,))
         cls.notes_edit = reverse('notes:edit', args=(cls.note.slug,))
         cls.notes_delete = reverse('notes:delete', args=(cls.note.slug,))
@@ -38,11 +38,10 @@ class TestRoutes(TestCase):
             cls.notes_edit,
             cls.notes_delete,
             notes_home,
-            users_login,
+            cls.users_login,
             users_logout,
             users_signup,
         )
-        cls.LOGIN_URL = reverse('users:login')
 
     def test_for_author(self):
         """Автор может зайти на все страницы."""
@@ -53,15 +52,15 @@ class TestRoutes(TestCase):
 
     def test_for_reader(self):
         """Обычный юзер может зайти на все страницы кроме чужих заметок."""
-        for url in self.all_urls:
-            with self.subTest(user=self.reader, url=url):
-                response = self.reader_client.get(url)
-                urls = (
+        urls_when_wait_404 = (
                     self.notes_detail,
                     self.notes_edit,
                     self.notes_delete
                 )
-                if url in urls:
+        for url in self.all_urls:
+            with self.subTest(user=self.reader, url=url):
+                response = self.reader_client.get(url)
+                if url in urls_when_wait_404:
                     self.assertEqual(
                         response.status_code,
                         HTTPStatus.NOT_FOUND
@@ -80,10 +79,10 @@ class TestRoutes(TestCase):
             self.notes_delete,
         )
         for url in self.all_urls:
-            with self.subTest():
-                redirect_url = f'{self.LOGIN_URL}?next={url}'
+            with self.subTest(url=url):
+                redirect_url = f'{self.users_login}?next={url}'
                 response = self.client.get(url)
-                if (url) in urls_for_authorized_user:
+                if url in urls_for_authorized_user:
                     self.assertRedirects(response, redirect_url)
                 else:
                     self.assertEqual(response.status_code, HTTPStatus.OK)
